7e2b23abb3147fbb390086cd463fd58404c8089d
azandrew-sidoine <azandrewdevelopper@gmail.com>
2023-08-16 19:28:43 +0000

    Added version 15+ compatible functional guard

--- guards/index.ts
+++ guards/index.ts
@@ -1,4 +1,16 @@
-export { AuthGuardService, TokenCanAnyGuard, TokenCanGuard } from "./auth.guard";
+export {
+  AuthGuardService,
+  TokenCanAnyGuard,
+  TokenCanGuard,
+} from "./auth.guard";
 export { AuthClientInterceptor as ClientAuthorizationInterceptor } from "../core/strategies/local/interceptors.guard";
 export { UnAuthorizedInterceptor as UnAuthorizedResponseInterceptorGuard } from "../http/unauthorized-interceptor";
 
+export {
+  canActivate,
+  tokenCanActivate,
+  tokenCanAnyActivate,
+  tokenCanAnyMatch,
+  tokenCanMatch,
+  canActivateChild
+} from "./auth-v15.guard";
