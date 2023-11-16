6a6e6940e39d1851c5ada514dc4b3fc11123817c
marcusholloway17 <loganjean17@gmail.com>
2023-11-16 13:22:23 +0000

    add forgot password translation

--- ui/common-strings/default.ts
+++ ui/common-strings/default.ts
@@ -13,10 +13,12 @@ export const defaultCommonStrings = {
   loginBtnText: "Login",
   authenticationFailed:
     "Authentication failed, Please check your credentials and retry.",
-  authorizationError: 'Authorization Error, You do not have access to the application.',
+  authorizationError:
+    "Authorization Error, You do not have access to the application.",
   authenticating: "Authenticating, please wait...",
   loginMaintext: "Welcome on",
   loginSubText: "Access your workspace with your credentials",
   successful: "You are successfully logged in. Redirecting ...",
   registerBtnText: "Register",
+  PasswordForgotBtnText: "Mot de passe oublié",
 };
